38679130e08f5b9c7724ff6460ceb5b2a414caeb
mandaapag03 <paganiamanda791@gmail.com>
2022-01-10 12:03:08 -0300

    Adiciona videoAulaMatrizes/
    Esse commit adiciona Aula1MatrizesVideo.java,arquivo da primeira aula de
    4 aulas do canal Loiane Groner. Vídeo: Curso Java 20 matrizes (arrays
      multidimensionais). Esse commit contêm:
    
       -arquivo .java
       -arquivo .class

---
+++ src/arrays/matriz/videoAulaMatrizes/Aula1MatrizesVideo.java
@@ -0,0 +1,23 @@
+package arrays.matriz.videos;
+
+public class Aula1MatrizesVideo {
+    public static void main(String[] args) {
+        
+        double notasDoAluno[][] = new double[3][4];
+        
+        notasDoAluno[0][0] = 10;
+        notasDoAluno[0][1] = 6.4;
+        notasDoAluno[0][2] = 9.8;
+        notasDoAluno[0][3] = 10;
+        
+        notasDoAluno[1][0] = 9;
+        notasDoAluno[1][1] = 6;
+        notasDoAluno[1][2] = 8.6;
+        notasDoAluno[1][3] = 5.4;
+        
+        notasDoAluno[2][0] = 1.5;
+        notasDoAluno[2][1] = 5.7;
+        notasDoAluno[2][2] = 4.9;
+        notasDoAluno[2][3] = 6;
+    }
+}
