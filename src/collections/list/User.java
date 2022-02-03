f2d0550652578f762f7cd9a922429c7bdf152d0f
mandaapag03 <paganiamanda791@gmail.com>
2022-02-02 22:39:15 -0300

    Adiciona anotações sobre List

---
+++ src/collections/list/User.java
@@ -0,0 +1,42 @@
+package collections.list;
+
+public class User {
+    
+    String nome;
+
+    User(String nome){
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    
+}
---
+++ src/collections/list/lista.java
@@ -0,0 +1,41 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lista {
+    public static void main(String[] args) {
+        List<User> list = new ArrayList<>();
+
+        // Instanciar objeto da lista
+        User u1 = new User("Gustavo");
+        list.add(u1);
+        list.add(new User("Amanda"));
+        list.add(new User("Gabriela"));
+        list.add(new User("Larissa"));
+        list.add(new User("Júlio"));
+
+        for(User u : list){
+            System.out.println(u.nome);
+        }
+
+        // Acessar pelo índex:
+        System.out.println("\nAcessar pelo índex:");
+        System.out.println(list.get(2).nome);
+        System.out.println(list.get(4)); // Com método toString na Classe User (toString implícito)
+        System.out.println(list.get(2).toString()); // (toString explícito)
+        System.out.println();
+        for(User x: list){
+            System.out.println(x); // (toString implícito)
+        }
+        
+        // Remover item da lista:
+        System.out.println("\nRemover item da lista:");
+        System.out.println(list.remove(new User("Amanda")));
+        System.out.println(list.remove(3));
+        System.out.println();
+        for(User z: list){
+            System.out.println(z); // (toString implícito)
+        }
+    }
+}
